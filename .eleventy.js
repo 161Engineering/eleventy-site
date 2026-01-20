9141c2afee274e5b2fbebc333024f7d89b01761a
Developer <dev@example.com>
2026-01-20 16:52:40 +0300

    Обновлен для России

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
-module.exports = function(eleventyConfig) {
-  return {
-    dir: {
-      output: "docs"
-    }
-  };
+module.exports = function (eleventyConfig) {
+    return {
+        dir: {
+            output: "docs"
+        }
+    };
 };
